0d7a77c0d5e6c2cf31ac5a3747a238fe6a89488d
Saiful Islam <https://github.com/Ferrari2003>
2022-11-26 12:28:57 +0600

    aded commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,9 +10,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wjha8kd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const categoriesCollection = client.db('bikeResale').collection('categories');
+
+        app.get('/category', async(req, res)=> {
+            const query= {};
+            const options = await categoriesCollection.find(query).toArray();
+            res.send(options)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+
 app.get('/', (req, res) => {
     res.send('server running')
 });
+
 
 app.listen(port, () => {
     console.log(`backEnd Server is Running${port}`)
